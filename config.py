2b10b5515129d08c30c63af93868edf0eecefc10
Samuthra <samuthra@stackbill.com>
2025-11-21 17:12:20 +0530

    initial setup

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+STACKBILL_API_KEY = os.getenv("STACKBILL_API_KEY")
+STACKBILL_SECRET_KEY = os.getenv("STACKBILL_SECRET_KEY")
+STACKBILL_BASE_URL = os.getenv("STACKBILL_BASE_URL", "http://demo.stackbill.com")
+
+if not STACKBILL_API_KEY or not STACKBILL_SECRET_KEY:
+    raise RuntimeError("Stackbill API credentials missing in environment variables")
